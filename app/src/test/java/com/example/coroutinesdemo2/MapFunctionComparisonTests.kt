3dd1488a2c7fcf2011ecc4e5e0121a92fc2d6b9f
Egidijus Gecius <e.gecius@gmail.com>
2024-10-07 17:13:51 +0100

    add test: mapLatest cancels transformation, once new value is by the original flow

---
+++ app/src/test/java/com/example/coroutinesdemo2/MapFunctionComparisonTests.kt
@@ -0,0 +1,28 @@
+package com.example.coroutinesdemo2
+
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class MapFunctionComparisonTests {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `mapLatest cancels transformation, once new value is by the original flow`() = runTest {
+        val flow = flow {
+            emit(1)
+            delay(100)
+            emit(2)
+        }
+
+        flow.mapLatest {
+            delay(101)
+            it * 3
+        }.toList() shouldBe listOf(6) // '1' transformation is cancelled
+    }
+}
